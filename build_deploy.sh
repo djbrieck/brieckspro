fafb8271eab32d750d88dbacf2b5481810f43861
Daniel J Brieck Jr <djbrieck@gmail.com>
2025-05-17 08:39:53 -0700

    Updating brieckspro deploy script to use IPFS cluster commands

--- build_deploy.sh
+++ build_deploy.sh
@@ -8,14 +8,14 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Attempting to add public directory to ipfs"
-PUBLIC_HASH=$(ipfs add -rQ public/ )
+PUBLIC_HASH=$(ipfs-cluster-ctl add -rQ public/ )
 if [ $? -ne 0 ]; then
-    echo  "Add to IPFS failed!"
+    echo  "Add to IPFS Cluster failed!"
     exit 2
 fi
 
 echo "Received public hash of: $PUBLIC_HASH so lets try to update IPNS to it"
-ipfs name publish -key=bprokey $PUBLIC_HASH 
+ipfs name publish -key=bprokey "$PUBLIC_HASH"
 if [ $? -ne 0 ]; then
     echo  "IPFS Publish to IPNS failed"
     exit 3
